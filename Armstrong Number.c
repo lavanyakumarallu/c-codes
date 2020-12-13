dae59ca82c9a52f431e0b18b4c676322033ded60
lavanyakumarallu <42909214+lavanyakumarallu@users.noreply.github.com>
2020-12-13 08:43:54 +0530

    Update Armstrong Number.c

--- Armstrong Number.c
+++ Armstrong Number.c
@@ -1,17 +1,46 @@
 #include<stdio.h>
 int main(){
-	int a, n, t, b = 0;
+	int a, n, t, count = 0, b = 0;
 	printf("Enter the Number: \n");
 	scanf("%d", &n);
 	t = n;
-	printf("\nGiven number is %d\n", t);
-	while (n > 0){
-		a = n % 10;
-		b = b + (a * a * a);
-		n = n / 10;
+	while(t != 0){
+		t = t/10;
+		count++;
 	}
-	if (t == b)
-	printf("%d is an Armstrong Number.", t);
+	t = n;
+	printf("\nGiven number is %d\n", t);
+	while (t > 0){
+		a = t % 10;
+		int cnt = count, mul = 1;
+		while(cnt != 0){
+			mul = mul * a;
+			cnt--;
+		}
+		b = b + mul;
+		cnt = count;
+		t = t / 10;
+		mul = 1;
+	}
+	if (n == b)
+	printf("%d is an Armstrong Number.", n);
 	else
-	printf("%d is not an Armstrong Number.", t);
+	printf("%d is not an Armstrong Number.", n);
 }
+
+/*
+OUTPUT ==>
+Ex. 1)-->
+Enter the Number:
+371
+
+Given number is 371
+371 is an Armstrong Number.
+
+Ex. 2)-->
+Enter the Number:
+569
+
+Given number is 569
+569 is not an Armstrong Number.
+*/
